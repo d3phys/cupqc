e6735b04a620684e9f8663f3d4c886f71132d52b
Nikita Tsybliyenko <nikita.dzer.g@gmail.com>
2024-12-06 18:02:40 +0300

    [Dev] Add Kyber768 tests.

--- test/kyber/build-keygen.sh
+++ test/kyber/build-keygen.sh
@@ -2,6 +2,6 @@
 
 # g++ -o keygen-test keygen-test.cc -O3 -L ../../cuda/lib -lpqcrystals_kyber512_cuda -L ../../ref/kyber/ref/lib/ -lpqcrystals_kyber512_ref -lpqcrystals_fips202_ref
 #g++ -o keygen-perf keygen-perf.cc -O3 -L ../../cuda/lib -lpqcrystals_kyber512_cuda -L ../../ref/kyber/ref/lib/ -lpqcrystals_kyber512_ref -lpqcrystals_fips202_ref
-g++ -o keygen-perf keygen-perf.cc -O3 -L ../../cuda/lib -lpqcrystals_kyber768_cuda -L ../../ref/kyber/avx2 -lpqcrystals_kyber768_avx2 -lpqcrystals_fips202_ref -lpqcrystals_fips202x4_avx2
+g++ -o keygen-perf keygen-perf.cc -O3 -L ../../cuda/lib -lpqcrystals_kyber768_cuda -L ../../ref/kyber/avx2 -lpqcrystals_kyber768_avx2 -lpqcrystals_fips202_ref -lpqcrystals_fips202x4_avx2 -std=c++17
 
 # LD_LIBRARY_PATH="../../cuda/lib;../../ref/kyber/ref/lib" ./a.out
--- test/kyber/keygen-perf.cc
+++ test/kyber/keygen-perf.cc
@@ -6,6 +6,9 @@
 #include <assert.h>
 #include <chrono>
 #include <iostream>
+#include <memory>
+#include <functional>
+#include <vector>
 
 #include "test-api.hh"
 
@@ -18,55 +21,74 @@ void randombytes(uint8_t *out, size_t outlen) {}
 }
 
 int pqcrystals_kyber768_cuda_keypair(uint8_t *pk, uint8_t *sk, const uint8_t *coins, uint32_t keypair_count);
+int pqcrystals_kyber768_cuda_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk, const uint8_t *coins, uint32_t keypair_count);
+int pqcrystals_kyber768_cuda_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk, uint32_t keypair_count);
 
-int kyberCudaKeyPairs( uint8_t *pk, uint8_t *sk, const uint8_t *coins, uint32_t keypair_count)
+using KyberKeyPair = int( uint8_t *pk, uint8_t *sk, const uint8_t *coins, uint32_t keypair_count);
+using KyberEnc = int( uint8_t *ct, uint8_t *ss, const uint8_t *pk, const uint8_t *coins, uint32_t keypair_count);
+using KyberDec = int( uint8_t *ss, const uint8_t *ct, const uint8_t *sk, uint32_t keypair_count);
+
+int kyberCudaKeyPair( uint8_t *pk, uint8_t *sk, const uint8_t *coins, uint32_t keypair_count)
 {
-    // Coins are the same
-    uint32_t step = 128 * 4;
-
-    for ( uint32_t i = 0; i < (keypair_count / step); ++i )
-    {
-        uint8_t* pub = (pk + kyberPublicKeyBytes * i * step);
-        uint8_t* sec = (sk + kyberSecretKeyBytes * i * step);
-        pqcrystals_kyber768_cuda_keypair( pub, sec, coins, step);
-    }
-
+    pqcrystals_kyber768_cuda_keypair( pk, sk, coins, keypair_count);
     return 0;
 }
 
-//int kyberRefKeyPairs( uint8_t *pk, uint8_t *sk, const uint8_t *coins, uint32_t keypair_count)
-//{
-//    for ( uint32_t i = 0; i < keypair_count; ++i )
-//    {
-//        uint8_t* pub = (pk + kyberPublicKeyBytes * i);
-//        uint8_t* sec = (sk + kyberSecretKeyBytes * i);
-//        pqcrystals_kyber512_ref_keypair_derand( pub, sec, coins);
-//    }
-//
-//    return 0;
-//}
-
-int kyberRefKeyPairs( uint8_t *pk, uint8_t *sk, const uint8_t *coins, uint32_t keypair_count)
+int kyberRefKeyPair( uint8_t *pk, uint8_t *sk, const uint8_t *coins, uint32_t keypair_count)
 {
     for ( uint32_t i = 0; i < keypair_count; ++i )
     {
         uint8_t* pub = (pk + kyberPublicKeyBytes * i);
         uint8_t* sec = (sk + kyberSecretKeyBytes * i);
-        pqcrystals_kyber768_avx2_keypair_derand( pub, sec, coins);
+        const uint8_t* cns = (coins + kyberKeyPairCoinBytes * i);
+        pqcrystals_kyber768_avx2_keypair_derand( pub, sec, cns);
     }
 
     return 0;
 }
 
-const uint32_t nKeys = 128 * 1000;
-
-int testKeygen()
+int kyberCudaEnc( uint8_t *ct, uint8_t *ss, const uint8_t *pk, const uint8_t *coins, uint32_t keypair_count)
 {
-    uint8_t* publicKeysRef = (uint8_t*)malloc( kyberPublicKeyBytes * nKeys);
-    uint8_t* secretKeysRef = (uint8_t*)malloc( kyberSecretKeyBytes * nKeys);
+    pqcrystals_kyber768_cuda_enc( ct, ss, pk, coins, keypair_count);
+    return 0;
+}
 
-    uint8_t* publicKeysCuda = (uint8_t*)malloc( kyberPublicKeyBytes * nKeys);
-    uint8_t* secretKeysCuda = (uint8_t*)malloc( kyberSecretKeyBytes * nKeys);
+int kyberRefEnc( uint8_t *ct, uint8_t *ss, const uint8_t *pk, const uint8_t *coins, uint32_t keypair_count)
+{
+    for ( uint32_t i = 0; i < keypair_count; ++i )
+    {
+        uint8_t* cph = (ct + kyberCipherTextBytes * i);
+        uint8_t* shr = (ss + kyberBytes * i);
+        const uint8_t* pub = (pk + kyberPublicKeyBytes * i);
+        const uint8_t* cns = (coins + kyberEncCoinBytes * i);
+        pqcrystals_kyber768_avx2_enc_derand( cph, shr, pub, cns);
+    }
+
+    return 0;
+}
+
+int kyberCudaDec( uint8_t *ss, const uint8_t *ct, const uint8_t *sk, uint32_t keypair_count)
+{
+    pqcrystals_kyber768_cuda_dec( ss, ct, sk, keypair_count);
+    return 0;
+}
+
+int kyberRefDec( uint8_t *ss, const uint8_t *ct, const uint8_t *sk, uint32_t keypair_count)
+{
+    for ( uint32_t i = 0; i < keypair_count; ++i )
+    {
+        uint8_t* shr = (ss + kyberBytes * i);
+        const uint8_t* cph = (ct + kyberCipherTextBytes * i);
+        const uint8_t* sec = (sk + kyberSecretKeyBytes * i);
+        pqcrystals_kyber768_avx2_dec( shr, cph, sec);
+    }
+
+    return 0;
+}
+
+std::unique_ptr<uint8_t[]> createCoins(const int nCoins, int coinBytes)
+{
+    assert(coinBytes <= kyberKeyPairCoinBytes);
 
     uint8_t keyPairCoins[kyberKeyPairCoinBytes] = {
         0xcb, 0x12, 0x61, 0xa8, 0xcf, 0x85, 0xa4, 0x8b, 0x5d, 0x37, 0xc1, 0x00, 0xb6, 0xb0, 0x2c, 0xfb,
@@ -75,46 +97,190 @@ int testKeygen()
         0x1f, 0xe5, 0xd3, 0x21, 0xbb, 0xcf, 0x6d, 0x3c, 0x98, 0xb5, 0x35, 0xc4, 0x74, 0xae, 0x1a, 0xb0,
     };
     
-    uint8_t* coins = (uint8_t*)malloc(sizeof(keyPairCoins) * nKeys);
-    assert(coins);
+    std::unique_ptr<uint8_t[]> coins = std::make_unique<uint8_t[]>(coinBytes * nCoins);
 
-    for ( int i = 0; i < nKeys; i++ )
+    for ( int i = 0; i < nCoins; i++ )
     {
-        memcpy(coins + i * sizeof(keyPairCoins), keyPairCoins, sizeof(keyPairCoins));    
+        memcpy(coins.get() + i * coinBytes, keyPairCoins, coinBytes);
     }
+
+    return coins;
+}
+
+std::pair<std::unique_ptr<uint8_t[]>, std::unique_ptr<uint8_t[]>>
+runKyberKeyPair(int nKeys, KyberKeyPair kyberKeyPair, double* duration = nullptr) {
+    auto publicKeys = std::make_unique<uint8_t[]>( kyberPublicKeyBytes * nKeys);
+    auto secretKeys = std::make_unique<uint8_t[]>( kyberSecretKeyBytes * nKeys);
+    auto coins = createCoins(nKeys, kyberKeyPairCoinBytes);
+
+    auto startRef = std::chrono::high_resolution_clock::now();
+    kyberKeyPair( publicKeys.get(), secretKeys.get(), coins.get(), nKeys);
+    auto endRef   = std::chrono::high_resolution_clock::now();
+
+    if ( duration ) 
+    {
+        std::chrono::duration<double> diff = endRef - startRef;
+        *duration = diff.count();
+    }
+
+    return {std::move(publicKeys), std::move(secretKeys)};
+}
+
+std::pair<std::unique_ptr<uint8_t[]>, std::unique_ptr<uint8_t[]>>
+runKyberEnc(const uint8_t* publicKeys, int nKeys, KyberEnc kyberEnc, double* duration = nullptr) {
+    auto cipherTexts = std::make_unique<uint8_t[]>( kyberCipherTextBytes * nKeys);
+    auto sharedSecrets = std::make_unique<uint8_t[]>( kyberBytes * nKeys);
+    auto coins = createCoins(nKeys, kyberEncCoinBytes);
+
+    auto startRef = std::chrono::high_resolution_clock::now();
+    kyberEnc( cipherTexts.get(), sharedSecrets.get(), publicKeys, coins.get(), nKeys);
+    auto endRef   = std::chrono::high_resolution_clock::now();
+
+    if ( duration ) 
+    {
+        std::chrono::duration<double> diff = endRef - startRef;
+        *duration = diff.count();
+    }
+
+    return {std::move(cipherTexts), std::move(sharedSecrets)};
+}
+
+std::unique_ptr<uint8_t[]>
+runKyberDec(const uint8_t* cipherTexts, const uint8_t* secretKeys, int nKeys, KyberDec kyberDec, double* duration = nullptr) {
+    auto sharedSecrets = std::make_unique<uint8_t[]>( kyberBytes * nKeys);
 
     // Alice generates a public key
     auto startRef = std::chrono::high_resolution_clock::now();
-    kyberRefKeyPairs(  publicKeysRef, secretKeysRef, keyPairCoins, nKeys);
+    kyberDec( sharedSecrets.get(), cipherTexts, secretKeys, nKeys);
     auto endRef   = std::chrono::high_resolution_clock::now();
 
-    auto startCuda = std::chrono::high_resolution_clock::now();
-    kyberCudaKeyPairs( publicKeysCuda, secretKeysCuda, coins, nKeys);
-    auto endCuda = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<double> diffRef = endRef - startRef;
-    std::chrono::duration<double> diffCuda = endCuda - startCuda;
-
-    std::cout << "Ref. time: " << diffRef.count() << "s\n";
-    std::cout << "Cuda time: " << diffCuda.count() << "s\n";
-
-    if ( memcmp( publicKeysRef, publicKeysCuda, nKeys * kyberPublicKeyBytes) ) 
+    if ( duration ) 
     {
-        printf("Public Keys dismatch\n");
+        std::chrono::duration<double> diff = endRef - startRef;
+        *duration = diff.count();
+    }
+
+    return sharedSecrets;
+}
+
+int testKeygen(int nKeys)
+{
+    double durRef = 0;
+    double durCuda = 0;
+
+    auto [publicKeysRef, secretKeysRef] = runKyberKeyPair(nKeys, kyberRefKeyPair, &durRef);
+    auto [publicKeysCuda, secretKeysCuda] = runKyberKeyPair(nKeys, kyberCudaKeyPair, &durCuda);
+
+    std::cout << "Ref. time: " << durRef << "s\n";
+    std::cout << "Cuda time: " << durCuda << "s\n";
+
+    if ( memcmp( publicKeysRef.get(), publicKeysCuda.get(), nKeys * kyberPublicKeyBytes) ) 
+    {
+        std::cerr << "Public Keys mismatch\n";
         return 1;
     }
 
-    if ( memcmp( secretKeysRef, secretKeysCuda, nKeys * kyberSecretKeyBytes) ) 
+    if ( memcmp( secretKeysRef.get(), secretKeysCuda.get(), nKeys * kyberSecretKeyBytes) ) 
     {
-        printf("Secret Keys dismatch\n");
+        std::cerr << "Secret Keys mismatch\n";
         return 2;
     }
 
     return 0;
 }
 
-int main()
+int testEnc(int nKeys) {
+    double durRef = 0;
+    double durCuda = 0;
+
+    auto [publicKeysRef, _] = runKyberKeyPair(nKeys, kyberRefKeyPair);
+
+    auto [cipherTextsRef, sharedSecretsRef] = runKyberEnc( publicKeysRef.get(), nKeys, kyberRefEnc, &durRef);
+    auto [cipherTextsCuda, sharedSecretsCuda] = runKyberEnc( publicKeysRef.get(), nKeys, kyberCudaEnc, &durCuda);
+
+    std::cout << "Ref. time: " << durRef << "s\n";
+    std::cout << "Cuda time: " << durCuda << "s\n";
+
+    if ( memcmp( cipherTextsRef.get(), cipherTextsCuda.get(), nKeys * kyberCipherTextBytes) ) 
+    {
+        std::cerr << "Cipher Texts mismatch\n";
+        return 1;
+    }
+
+    if ( memcmp( sharedSecretsRef.get(), sharedSecretsCuda.get(), nKeys * kyberBytes) ) 
+    {
+        std::cerr << "Shared Secrets mismatch\n";
+        return 2;
+    }
+
+    return 0;
+}
+
+int testDec(int nKeys) {
+    double durRef = 0;
+    double durCuda = 0;
+
+    auto [publicKeysRef, secretKeysRef] = runKyberKeyPair(nKeys, kyberRefKeyPair);
+    auto [cipherTextsRef, _] = runKyberEnc( publicKeysRef.get(), nKeys, kyberRefEnc);
+
+    auto sharedSecretsRef = runKyberDec( cipherTextsRef.get(), secretKeysRef.get(), nKeys, kyberRefDec, &durRef);
+    auto sharedSecretsCuda = runKyberDec( cipherTextsRef.get(), secretKeysRef.get(), nKeys, kyberCudaDec, &durCuda);
+
+    std::cout << "Ref. time: " << durRef << "s\n";
+    std::cout << "Cuda time: " << durCuda << "s\n";
+
+    if ( memcmp( sharedSecretsRef.get(), sharedSecretsCuda.get(), nKeys * kyberBytes) ) 
+    {
+        std::cerr << "Shared Secrets mismatch\n";
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(int argc, const char *argv[])
 {
-    assert( testKeygen() == 0 );
+    const uint32_t kDefaultNKeys = 128 * 1000;
+    const std::vector<std::function<int(int)>> kDefaultTests = {testKeygen, testEnc, testDec};
+
+    uint32_t nKeys = kDefaultNKeys;
+    std::vector<std::function<int(int)>> tests = kDefaultTests;
+
+    if ( argc > 1 )
+    {
+        nKeys = std::atoi(argv[1]);
+
+        if ( argc > 2 )
+        {
+            std::string testName = argv[2];
+            tests.clear();
+
+            if ( testName == "all" )
+            {
+                tests = kDefaultTests;
+            } else if ( testName == "keygen" )
+            {
+                tests.push_back(testKeygen);
+            } else if ( testName == "enc" )
+            {
+                tests.push_back(testEnc);
+            } else if ( testName == "dec" )
+            {
+                tests.push_back(testDec);
+            } else
+            {
+                std::cerr << "Unknown test " << testName;
+                assert(0);
+            }
+        }
+    }
+
+    for ( auto &&test : tests )
+    {
+        int result = test(nKeys);
+        assert(result == 0);
+    }
+
+    return 0;
 }
 
